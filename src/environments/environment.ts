edeab050495e94d386211cdc8aa26cead04c8532
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2025-08-11 09:13:37 +0530

    Require authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3001/'
+    apiUrl: 'http://localhost:3001/',
+    signInRedirectUrl: 'http://localhost:1420/',
+    clientID: '<client-id>',
+    baseUrl: 'https://api.asgardeo.io/t/<org-name>',
+    scope: ['openid', 'profile']
 };
