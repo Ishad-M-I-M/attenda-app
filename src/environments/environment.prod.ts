0e1cf391ea9e94232301b4e5d3a8e36898398ad7
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2025-09-10 05:58:33 +0530

    Fix configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: process.env['API_URL'],
-    signInRedirectUrl: process.env['VERCEL_URL'],
-    clientID: process.env['ASGARDEO_CLIENT_ID'],
-    baseUrl: process.env['ASGARDEO_BASE_URL'],
+    apiUrl: 'API_URL',
+    signInRedirectUrl: 'VERCEL_URL',
+    clientID: 'ASGARDEO_CLIENT_ID',
+    baseUrl: 'ASGARDEO_BASE_URL',
     scope: ['openid', 'profile']
 };
